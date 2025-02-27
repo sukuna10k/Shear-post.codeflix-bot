ce759d0dfafa40914ce7f9c89f27513d0c372120
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-27 08:02:14 +0000

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 24817837
 API_HASH     = "acd9f0cc6beb08ce59383cf250052686"
 BOT_TOKEN    = "7902142337:AAGsOZJ4Xs4REbr8HHezcP-ZY2aM7ZerDwA"
-SESSION      = "post_shear"
+SESSION      = ""
 DATABASE_URI = "mongodb+srv://tgbot:4KzEdxEl4YldwwFR@tg.vr8ef.mongodb.net/?retryWrites=true&w=majority&appName=Tg"
 LOG_CHANNEL  = -1002376378205
 ADMIN        = 7428552084
